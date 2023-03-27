18ff7577c3a94e8bee2e2ad0c0f8993c698fb46e
Aleksandr Khomenko <khomenkos196@gmail.com>
2023-03-27 13:51:33 +0300

    Created project.

---
+++ TestTask-Voio/Controller/Detail/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  TestTask-Voio
+//
+//  Created by  Sasha Khomenko on 27.03.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
